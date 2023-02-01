7e990562809f96b55d7b3f1c240d6a6dac875187
ZAIN IQBAL <124054465+Zaini04@users.noreply.github.com>
2023-02-01 20:04:14 +0500

    Add files via upload

---
+++ Comperision operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>//preprocessor directive 
+using namespace std;//header file
+int main()//main function
+{//opening brace
+	int a=10,b=5;//declare value to a and b
+	cout<<(a>b);//this show that a greater than b
+	cout<<(a<b);//this show that a less than b
+	cout<<(a<=b);//this show that a lesd than is equal to b
+	cout<<(a>=b);//this show taht a greater than is equal to b
+	cout<<(a==b);//this show that a is equal to b
+	cout<<(a!=b);//this show that a is not equal to b
+	return 0;
+}//closing brace
